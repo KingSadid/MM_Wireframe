192965317ac40cf21fcd17d1cb0053d93f40ec49
Sadid Acosta <SadiSex07@gmail.com>
2026-02-23 15:31:44 -0500

    Corrections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:app1_mm/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const AppBarClass());
+  testWidgets('La app carga correctamente los elementos del diseño', (WidgetTester tester) async {
+    
+    await tester.pumpWidget(const SketchCloneApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsNothing);
+
+    expect(find.text('Explore Categories'), findsOneWidget);
+
+    expect(find.byIcon(Icons.pause), findsOneWidget);
+
+    expect(find.byIcon(Icons.play_circle_outline), findsOneWidget);
   });
 }
